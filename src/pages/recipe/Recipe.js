d328b8f1e9b6c7bb88cb465a2f537196c137163c
pmita <panos.mita2@outlook.com>
2021-11-09 15:44:13 +0000

    Feat/Dynamic routing for individual products

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Recipe.css';
+// ROUTER
+import { useParams } from 'react-router';
+// HOOKS
+import { useFetch } from '../../hooks/useFetch';
 
 const Recipe = () => {
+    const { id } = useParams(); //returns an object
+    const url = 'http://localhost:3000/recipes/' + id;
+    const { error, isPending, data: recipe} = useFetch(url);
+
     return(
-        <h1>Hello from Recipe Page component</h1>
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {recipe && <h1>{recipe.title}</h1>}
+        </div>
     );
 };
 
