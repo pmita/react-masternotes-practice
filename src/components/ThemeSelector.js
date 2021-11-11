88d07416daf71f0993b7173132601b47380594b8
pmita <panos.mita2@outlook.com>
2021-11-11 09:56:29 +0000

    Feat/Dark-Light mode button introduced

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './ThemeSelector.css';
 //HOOKS
 import { useTheme } from '../hooks/useTheme';
+//ASSETS
+import modeIcon from '../assets/mode-icon.svg';
 
 const themeColors = ['#58249c', '#249c6b', '#b70233'];
 
 const ThemeSelector = () => {
-    const { changeColor } = useTheme();
+    const { changeColor, changeMode, mode } = useTheme();
+
+    const toggleMode = () => {
+        changeMode(mode=== 'dark' ? 'light' : 'dark');
+    }
     return(
         <div className='theme-selector'>
+            <div className='mode-toggle'>
+                <img 
+                    onClick={toggleMode}
+                    src={modeIcon} 
+                    alt='color toggle button'
+                    style={{ filter : mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}} 
+                />
+            </div>
             <div className='theme-buttons'>
                 {themeColors.map(color => (
                     <div 
